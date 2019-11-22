9433acd658bf4c65958c78388c9ef949a4e2b9d1
Pot <Potatooosss22@gmail.com>
2019-11-23 00:17:51 +0700

    post

---
+++ assets/script/navbarToggles.js
@@ -0,0 +1,11 @@
+$(function(){
+    var prev = 0;
+    var $window = $(window);
+    var nav = $('.nav');
+
+    $window.on('scroll', function(){
+    var scrollTop = $window.scrollTop();
+    nav.toggleClass('hide', scrollTop > prev);
+    prev = scrollTop;
+    })
+})
