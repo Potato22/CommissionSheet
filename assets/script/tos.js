1416a52764a9f2a20fee859595a2d24ec1b526a8
Potato22 <potatooosss22@gmail.com>
2022-08-28 23:48:48 +0700

    tostimerfix

--- assets/script/tos.js
+++ assets/script/tos.js
@@ -2,45 +2,45 @@ const ONE_DAY = 24 * 60 * 60 * 1000;
 const TOS_EXPIRY = 7 * ONE_DAY;
 
 function isTOSAccepted() {
-  const tosDate = localStorage.getItem('tosAcceptedAt');
+    const tosDate = localStorage.getItem('tosAcceptedAt');
 
 
-  return tosDate && (new Date(tosDate).getTime() - Date.now()) <= TOS_EXPIRY;
+    return tosDate && (Date.now() - new Date(tosDate).getTime()) <= TOS_EXPIRY;
 }
 
 function acceptTOS() {
-  localStorage.setItem('tosAcceptedAt', (new Date()).toISOString());
+    localStorage.setItem('tosAcceptedAt', (new Date()).toISOString());
 }
 
 
-$(function() {
-    $(".tosButton").click(function(e){
+$(function () {
+    $(".tosButton").click(function (e) {
         window.location.href = "tos.html";
     })
-    if(isTOSAccepted()) {
-        console.log("[tosDEBUG] tos already dismissed at "+localStorage.getItem('tosAcceptedAt'))
+    if (isTOSAccepted()) {
+        console.log("[tosDEBUG] tos already dismissed at " + localStorage.getItem('tosAcceptedAt'))
         $('.tosBar').addClass("accept");
         $('.tosBound').fadeOut("fast");
     } else {
-        console.log("[tosDEBUG] no written data")
+        console.log("[tosDEBUG] no written data or reset")
     }
 
 
-    $(".tosButton, .dismiss").click(function(){
+    $(".tosButton, .dismiss").click(function () {
         $('.tosBar').addClass("accept");
-        setTimeout(function(){
+        setTimeout(function () {
             $('.tosBound').fadeOut("fast");
         }, 100);
         acceptTOS();
     });
 
-    $( ".tosBound" ).click(function() {
-        $('.tosBar').addClass( "ohno" );
+    $(".tosBound").click(function () {
+        $('.tosBar').addClass("ohno");
         $('.tosBound').addClass("yikes");
-        setTimeout(function() {
-            $('.tosBar').removeClass( "ohno" );
+        setTimeout(function () {
+            $('.tosBar').removeClass("ohno");
             $('.tosBound').removeClass("yikes");
         }, 400);
     });
 });
-console.log("[tosDEBUG] tosDismissed:"+localStorage.getItem('tosAcceptedAt'))
+console.log("[tosDEBUG] tosDismissed:" + localStorage.getItem('tosAcceptedAt'))
