93e759ad03dba2af2b91338724b866f54e8dc5cb
Potato22 <potatooosss22@gmail.com>
2022-09-10 23:32:20 +0700

    minify

--- assets/script/zoomtrigger.js
+++ assets/script/zoomtrigger.js
@@ -1,15 +1,1 @@
-$(function() {
-    $(".imgsample").click(function(){
-        $(".zoomincontent").attr("src",$(this).attr('src'));
-        $(".zoomindimmer").fadeIn(200);
-        $(".zoomin").show();
-    });
-
-    $(".zoomincontent,.zoomin").click(function(){
-        $(".zoomincontent").attr("src", "");
-        $(".zoomindimmer").fadeOut(200);
-        $(".zoomin").hide();
-    });
-});
-//HOLY SHIT YOU MADE ME SPENT 3 HOURS JUST FOR THIS
-//WHAT THE FUCK IS ATTR
+$((function(){$(".imgsample").click((function(){$(".zoomincontent").attr("src",$(this).attr("src")),$(".zoomindimmer").fadeIn(200),$(".zoomin").show()})),$(".zoomincontent,.zoomin").click((function(){$(".zoomincontent").attr("src",""),$(".zoomindimmer").fadeOut(200),$(".zoomin").hide()}))}));
