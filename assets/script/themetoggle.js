bceaae2660941ba5b5fafd9b0480e5df5b85f1b2
Potato22 <potatooosss22@gmail.com>
2022-12-09 22:35:45 +0700

    slight design changes

--- assets/script/themetoggle.js
+++ assets/script/themetoggle.js
@@ -1,1 +1,32 @@
-const toggleSwitch=document.querySelector("#ttogle"),currentTheme=localStorage.getItem("theme");function switchTheme(t){t.target.checked?(document.documentElement.setAttribute("data-theme","light"),localStorage.setItem("theme","light"),$(".splash").addClass("splashanim"),setTimeout((function(){$(".splash").removeClass("splashanim")}),500),$(".theme-icon").text("dark_mode"),$(".togglecont").attr("title","Change to Dark Mode")):(document.documentElement.setAttribute("data-theme","dark"),localStorage.setItem("theme","dark"),$(".splash").addClass("splashanim"),setTimeout((function(){$(".splash").removeClass("splashanim")}),500),$(".theme-icon").text("light_mode"),$(".togglecont").attr("title","Change to Light Mode"))}currentTheme&&(document.documentElement.setAttribute("data-theme",currentTheme),"light"===currentTheme?(toggleSwitch.checked=!0,$(".theme-icon").text("dark_mode"),$(".togglecont").attr("title","Change to Dark Mode")):($(".theme-icon").text("light_mode"),$(".togglecont").attr("title","Change to Light Mode"))),toggleSwitch.addEventListener("change",switchTheme,!1);
+const toggleSwitch = document.querySelector("#ttogle"),
+    currentTheme = localStorage.getItem("theme");
+
+function switchTheme(t) {
+    t.target.checked ? (document.documentElement.setAttribute("data-theme", "light"), localStorage.setItem("theme", "light"), $(".invertIcon").css(
+        {
+            'filter': 'invert(0)'
+        }), $(".splash").addClass("splashanim"), setTimeout((function () {
+            $(".splash").removeClass("splashanim")
+        }), 500), $(".theme-icon").text("dark_mode"), $(".togglecont").attr("title", "Change to Dark Mode")) : (document.documentElement.setAttribute("data-theme", "dark"), localStorage.setItem("theme", "dark"), $(".invertIcon").css(
+            {
+                'filter': 'invert(1)'
+            }), $(".splash").addClass("splashanim"), setTimeout((function () {
+                $(".splash").removeClass("splashanim")
+            }), 500), $(".theme-icon").text("light_mode"), $(".togglecont").attr("title", "Change to Light Mode"))
+}
+if (currentTheme) {
+    document.documentElement.setAttribute("data-theme", currentTheme);
+
+    if (currentTheme === "light") {
+        toggleSwitch.checked = true;
+        $(".theme-icon").text("dark_mode");
+        $(".togglecont").attr("title", "Change to Dark Mode");
+        $(".invertIcon").css({ 'filter': 'invert(0)' });
+    } else {
+        $(".theme-icon").text("light_mode");
+        $(".togglecont").attr("title", "Change to Light Mode");
+        $(".invertIcon").css({ 'filter': 'invert(1)' });
+    }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
