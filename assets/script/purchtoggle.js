6465ff14675f1995a918df2a36b3ec0e06ff283c
potato22 <potatooosss22@gmail.com>
2024-01-15 14:40:14 +0700

    query machine broke

--- assets/script/purchtoggle.js
+++ assets/script/purchtoggle.js
@@ -3,7 +3,6 @@ purchcontainer.attr({
     'data-disableorder': disableOrders
 })
 const purchoff = purchcontainer.data('disableorder');
-const disablereason = "... The slots are out or is just closed, come back later!"
 if (purchoff == true) {
     purchcontainer.attr({
         'aria-label': disablereason,
