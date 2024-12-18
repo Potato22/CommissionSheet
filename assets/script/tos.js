a21ee80f2ac42a19d27bf6a0d5b1e2d47b93faa3
Potato22 <potatooosss22@gmail.com>
2024-12-19 00:17:50 +0700

    some price changes and slight design tweak

--- assets/script/tos.js
+++ assets/script/tos.js
@@ -1,1 +1,24 @@
-const ONE_DAY=864e5,TOS_EXPIRY=6048e5;function isTOSAccepted(){const t=localStorage.getItem("tosAcceptedAt");return t&&Date.now()-new Date(t).getTime()<=6048e5}function acceptTOS(){localStorage.setItem("tosAcceptedAt",(new Date).toISOString())}$((function(){$(".tosButton").click((function(t){window.location.href="tos.html"})),isTOSAccepted()&&($(".tosBar").addClass("accept"),$(".tosBound").fadeOut("fast")),$(".tosButton, .dismiss").click((function(){$(".tosBar").addClass("accept"),setTimeout((function(){$(".tosBound").fadeOut("fast")}),100),acceptTOS()})),$(".tosBound").click((function(){$(".tosBar").addClass("ohno"),$(".tosBound").addClass("yikes"),setTimeout((function(){$(".tosBar").removeClass("ohno"),$(".tosBound").removeClass("yikes")}),400)}))}));
+const ONE_DAY = 864e5,
+    TOS_EXPIRY = 6048e5;
+
+function isTOSAccepted() {
+    const t = localStorage.getItem("tosAcceptedAt");
+    return t && Date.now() - new Date(t).getTime() <= 6048e5
+}
+
+function acceptTOS() {
+    localStorage.setItem("tosAcceptedAt", (new Date).toISOString())
+}
+$((function () {
+    $(".tosButton").click((function (t) {
+        window.location.href = "tos.html"
+    })), isTOSAccepted() && ($(".tosBar").addClass("accept"), $(".tosBound").fadeOut("fast")), $(".tosButton, .dismiss").click((function () {
+        $(".tosBar").addClass("accept"), setTimeout((function () {
+            $(".tosBound").fadeOut("fast")
+        }), 100), acceptTOS()
+    })), $(".tosBound").click((function () {
+        $(".tosBar").addClass("ohno"), $(".tosBound").addClass("yikes"), setTimeout((function () {
+            $(".tosBar").removeClass("ohno"), $(".tosBound").removeClass("yikes")
+        }), 400)
+    }))
+}));
